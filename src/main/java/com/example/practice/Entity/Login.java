894b0b9b2d2b1b0fdfed035f353e66cdb6fcfbb5
Deepika-Prasad <prasad.deepika91@gmail.com>
2023-12-01 22:49:50 +0530

    initial commit

---
+++ src/main/java/com/example/practice/Entity/Login.java
@@ -0,0 +1,7 @@
+package com.example.practice.Entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Login {
+}
---
+++ src/main/java/com/example/practice/ontroller/LoginController.java
@@ -0,0 +1,18 @@
+package com.example.practice.ontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+//    @PostMapping(path = "/login")
+//    public String retailerDoc() {
+//        return new ModelAndView("registration");
+//    }
+//
+//    @GetMapping("/login")
+//    public String retailerDoc() {
+//        return new ModelAndView("login");
+    }
